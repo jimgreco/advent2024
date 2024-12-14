35da95998f8edabcb0e343114d8db7e7273f7dee
Jim Greco <jgreco@gmail.com>
2024-12-14 08:51:35 -0500

    Day 12

--- src/advent2024/Day12.java
+++ src/advent2024/Day12.java
@@ -2,83 +2,143 @@ package advent2024;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Path;
 
 /**
- * You're given a list of numbers.
+ * You're given a given of letters.
+ * Grid cells next to each other with the same letters form shapes.
+ *
+ * <p>Part 1: Sum the product of the area and perimeter for each shape.
+ * Solution: Two different shapes can share the same letter so first classify each shape with a unique identifier using
+ * a DFS.
+ * The area for each shape is the number of cells with that identifier.
+ * The perimeters for each shape is the number of times, for each cell in the shape, an adjacent cell (in each of the
+ * four directions) have different identifiers.
+ *
+ * <p>Part 2: Sum the product of the area and the number of sides for each shape.
+ * Solution: Do the same classification as part 1.
+ * Sides are calculated by doing corner detection. (Thanks Aaron!)
  */
 public class Day12 {
 
-    private static final long[] POW = new long[18];
-
-    static {
-        POW[0] = 1;
-        for (var i = 1; i < POW.length; i++) {
-            POW[i] = 10 * POW[i - 1];
-        }
-    }
+    private static final int[][] DIRS = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
     public static void main(String[] args) throws IOException {
         // read into a list
-        var input = Files.readString(java.nio.file.Path.of("resources/day11"));
-        var nums = Arrays.stream(input.split(" ")).mapToLong(Long::parseLong).toArray();
+        var input = Files.readAllLines(Path.of("resources/day12"));
+        var grid = new char[input.size()][];
+        for (var i = 0; i < grid.length; i++) {
+            grid[i] = input.get(i).toCharArray();
+        }
 
-        // Solution 1: 185894
-        var count = countForList(nums, 25);
-        System.out.println(count);
+        // Solution 1: 1573474
+        var sumProduct = doPart1(grid);
+        System.out.println(sumProduct);
 
-        // Solution 2: 221632504974231
-        count = countForList(nums, 75);
-        System.out.println(count);
+        // Solution 2: 966476
+        sumProduct = doPart2(grid);
+        System.out.println(sumProduct);
     }
 
-    private static long countForList(long[] list, int iterations) {
-        var memo = new HashMap<State, Long>();
-        var count = 0L;
-        for (var num : list) {
-            count += countForNum(num, iterations, memo);
-        }
-        return count;
-    }
+    private static long doPart1(char[][] grid) {
+        var classifiedGrid = new int[grid.length][grid[0].length];
+        var length = classify(grid, classifiedGrid);
+        var perimeter = new int[length];
+        var areas = new int[length];
 
-    private static long countForNum(long num, int iterations, Map<State, Long> memo) {
-        if (iterations == 0) {
-            // we've done every iteration, the number cannot be modified
-            return 1;
-        }
-
-        // unique state is the number we are modifiying and the number of iterations remaining
-        var state = new State(num, iterations);
-        var count = memo.get(state);
-        if (count != null) {
-            return count;
-        }
-
-        if (num == 0) {
-            // condition 1: num is 0, make 1
-            count = countForNum(1, iterations - 1, memo);
-        } else {
-            // count number of digits in num
-            var digits = 1;
-            while (num >= POW[digits]) {
-                digits++;
-            }
-
-            if (digits % 2 == 0) {
-                // condition 2: num has even number of digits, string split the number into two halves
-                count = countForNum(num / POW[digits / 2], iterations - 1, memo)
-                        + countForNum(num % POW[digits / 2], iterations - 1, memo);
-            } else {
-                // condition 3: otherwise, multiply num by 2024
-                count = countForNum(2024 * num, iterations - 1, memo);
+        for (var i = 0; i < classifiedGrid.length; i++) {
+            for (var j = 0; j < classifiedGrid[i].length; j++) {
+                var id = classifiedGrid[i][j];
+                // each cell adds to the area
+                areas[id - 1]++;
+                for (var dir : DIRS) {
+                    // look up, down, left, right for whether there is a diff in id with the current cell
+                    var adjacentRow = i + dir[0];
+                    var adjacentCol = j + dir[1];
+                    if (isValid(grid, adjacentRow, adjacentCol)) {
+                        // adjacent cell is in bounds, only a perimeter if different ids
+                        var adjacentId = classifiedGrid[adjacentRow][adjacentCol];
+                        if (id != adjacentId) {
+                            perimeter[id - 1]++;
+                        }
+                    } else {
+                        // adjacent cell is out of bounds, always a perimeter
+                        perimeter[id - 1]++;
+                    }
+                }
             }
         }
-
-        memo.put(state, count);
-        return count;
+        return sumProduct(perimeter, areas);
     }
 
-    private record State(long num, int remainingIterations) {}
+    private static long doPart2(char[][] grid) {
+        var cfGrid = new int[grid.length][grid[0].length];
+        var length = classify(grid, cfGrid);
+        var areas = new int[length];
+        var sides = new int[length];
+        var rows = cfGrid.length;
+        var cols = cfGrid[0].length;
+
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                var id = cfGrid[i][j];
+                var up = i - 1 >= 0 ? cfGrid[i - 1][j] : 0;
+                var upRight = i - 1 >= 0 && j + 1 < cols ? cfGrid[i - 1][j + 1] : 0;
+                var right = j + 1 < rows ? cfGrid[i][j + 1] : 0;
+                var downRight = i + 1 < rows && j + 1 < cols ? cfGrid[i + 1][j + 1] : 0;
+                var down = i + 1 < rows ? cfGrid[i + 1][j] : 0;
+                var downLeft = i + 1 < rows && j - 1 >= 0 ? cfGrid[i + 1][j - 1] : 0;
+                var left = j - 1 >= 0 ? cfGrid[i][j - 1] : 0;
+                var upLeft = i - 1 >= 0 && j - 1 >= 0 ? cfGrid[i - 1][j - 1] : 0;
+                sides[id - 1] += ((id != up && id != left) ? 1 : 0)
+                        + ((id == up && id == left && id != upLeft) ? 1 : 0)
+                        + ((id != down && id != left) ? 1 : 0)
+                        + ((id == down && id == left && id != downLeft) ? 1 : 0)
+                        + ((id != up && id != right) ? 1 : 0)
+                        + ((id == up && id == right && id != upRight) ? 1 : 0)
+                        + ((id != down && id != right) ? 1 : 0)
+                        + ((id == down && id == right && id != downRight) ? 1 : 0);
+                areas[id - 1]++;
+            }
+        }
+        return sumProduct(sides, areas);
+    }
+
+    private static int classify(char[][] grid, int[][] classified) {
+        var length = 0;
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid[i].length; j++) {
+                if (classified[i][j] == 0) {
+                    dfs(grid, i, j, classified, ++length);
+                }
+            }
+        }
+        return length;
+    }
+
+    private static void dfs(char[][] grid, int row, int col, int[][] classified, int id) {
+        var c = grid[row][col];
+        classified[row][col] = id;
+        for (var dir : DIRS) {
+            var adjacentRow = row + dir[0];
+            var adjacentCol = col + dir[1];
+            if (isValid(grid, adjacentRow, adjacentCol)
+                    && classified[adjacentRow][adjacentCol] == 0
+                    && grid[adjacentRow][adjacentCol] == c) {
+                dfs(grid, adjacentRow, adjacentCol, classified, id);
+            }
+        }
+    }
+
+    private static boolean isValid(char[][] grid, int row, int col) {
+        return row >= 0 && row < grid.length && col >= 0 && col < grid[row].length;
+    }
+
+    private static long sumProduct(int[] a, int[] b) {
+        var sumProduct = 0L;
+        for (var i = 0; i < a.length; i++) {
+            sumProduct += (long) a[i] * b[i];
+        }
+        return sumProduct;
+    }
 }
--- src/advent2024/Day13.java
+++ src/advent2024/Day13.java
@@ -3,142 +3,65 @@ package advent2024;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 /**
- * You're given a given of letters.
- * Grid cells next to each other with the same letters form shapes.
- *
- * <p>Part 1: Sum the product of the area and perimeter for each shape.
- * Solution: Two different shapes can share the same letter so first classify each shape with a unique identifier using
- * a DFS.
- * The area for each shape is the number of cells with that identifier.
- * The perimeters for each shape is the number of times, for each cell in the shape, an adjacent cell (in each of the
- * four directions) have different identifiers.
- *
- * <p>Part 2: Sum the product of the area and the number of sides for each shape.
- * Solution: Do the same classification as part 1.
- * Sides are calculated by doing corner detection. (Thanks Aaron!)
+ * Solution: Use Cramer's rule to solve a system of N equations with N variables.
+ * Floating point solutions are invalid so check the determinant against the variable numerators.
  */
 public class Day13 {
 
-    private static final int[][] DIRS = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-
     public static void main(String[] args) throws IOException {
-        // read into a list
+        // read into a list of buttons and prizes
         var input = Files.readAllLines(Path.of("resources/day13"));
-        var grid = new char[input.size()][];
-        for (var i = 0; i < grid.length; i++) {
-            grid[i] = input.get(i).toCharArray();
+        var machines = new ArrayList<Machine>();
+        for (var i = 0; i < input.size(); i += 4) {
+            machines.add(new Machine(parse(input.get(i)), parse(input.get(i + 1)), parse(input.get(i + 2))));
         }
 
-        // Solution 1: 1573474
-        var sumProduct = doPart1(grid);
-        System.out.println(sumProduct);
+        var total = calculate(machines, 0);
+        System.out.println(total);
 
-        // Solution 2: 966476
-        sumProduct = doPart2(grid);
-        System.out.println(sumProduct);
+        total = calculate(machines, 10000000000000L);
+        System.out.println(total);
     }
 
-    private static long doPart1(char[][] grid) {
-        var classifiedGrid = new int[grid.length][grid[0].length];
-        var length = classify(grid, classifiedGrid);
-        var perimeter = new int[length];
-        var areas = new int[length];
-
-        for (var i = 0; i < classifiedGrid.length; i++) {
-            for (var j = 0; j < classifiedGrid[i].length; j++) {
-                var id = classifiedGrid[i][j];
-                // each cell adds to the area
-                areas[id - 1]++;
-                for (var dir : DIRS) {
-                    // look up, down, left, right for whether there is a diff in id with the current cell
-                    var adjacentRow = i + dir[0];
-                    var adjacentCol = j + dir[1];
-                    if (isValid(grid, adjacentRow, adjacentCol)) {
-                        // adjacent cell is in bounds, only a perimeter if different ids
-                        var adjacentId = classifiedGrid[adjacentRow][adjacentCol];
-                        if (id != adjacentId) {
-                            perimeter[id - 1]++;
-                        }
-                    } else {
-                        // adjacent cell is out of bounds, always a perimeter
-                        perimeter[id - 1]++;
-                    }
-                }
+    private static long calculate(ArrayList<Machine> machines, long offset) {
+        var total = 0L;
+        for (var machine : machines) {
+            var sln = cramers(machine, offset);
+            if (sln != null) {
+                total += 3 * sln[0] + sln[1];
             }
         }
-        return sumProduct(perimeter, areas);
+        return total;
     }
 
-    private static long doPart2(char[][] grid) {
-        var cfGrid = new int[grid.length][grid[0].length];
-        var length = classify(grid, cfGrid);
-        var areas = new int[length];
-        var sides = new int[length];
-        var rows = cfGrid.length;
-        var cols = cfGrid[0].length;
-
-        for (var i = 0; i < rows; i++) {
-            for (var j = 0; j < cols; j++) {
-                var id = cfGrid[i][j];
-                var up = i - 1 >= 0 ? cfGrid[i - 1][j] : 0;
-                var upRight = i - 1 >= 0 && j + 1 < cols ? cfGrid[i - 1][j + 1] : 0;
-                var right = j + 1 < rows ? cfGrid[i][j + 1] : 0;
-                var downRight = i + 1 < rows && j + 1 < cols ? cfGrid[i + 1][j + 1] : 0;
-                var down = i + 1 < rows ? cfGrid[i + 1][j] : 0;
-                var downLeft = i + 1 < rows && j - 1 >= 0 ? cfGrid[i + 1][j - 1] : 0;
-                var left = j - 1 >= 0 ? cfGrid[i][j - 1] : 0;
-                var upLeft = i - 1 >= 0 && j - 1 >= 0 ? cfGrid[i - 1][j - 1] : 0;
-                sides[id - 1] += ((id != up && id != left) ? 1 : 0)
-                        + ((id == up && id == left && id != upLeft) ? 1 : 0)
-                        + ((id != down && id != left) ? 1 : 0)
-                        + ((id == down && id == left && id != downLeft) ? 1 : 0)
-                        + ((id != up && id != right) ? 1 : 0)
-                        + ((id == up && id == right && id != upRight) ? 1 : 0)
-                        + ((id != down && id != right) ? 1 : 0)
-                        + ((id == down && id == right && id != downRight) ? 1 : 0);
-                areas[id - 1]++;
-            }
-        }
-        return sumProduct(sides, areas);
+    private static Coord parse(String input) {
+        var pattern = Pattern.compile("X[+=](\\d+),\\s*Y[+=](\\d+)");
+        var matcher = pattern.matcher(input);
+        matcher.find();
+        return new Coord(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
     }
 
-    private static int classify(char[][] grid, int[][] classified) {
-        var length = 0;
-        for (var i = 0; i < grid.length; i++) {
-            for (var j = 0; j < grid[i].length; j++) {
-                if (classified[i][j] == 0) {
-                    dfs(grid, i, j, classified, ++length);
-                }
-            }
-        }
-        return length;
+    private static long[] cramers(Machine machine, long offset) {
+        // equations:
+        // buttonA.x (a) * n + buttonB.x (b) * m = prize.x (c)
+        // buttonA.y (d) * n + buttonB.y (e) * m = prize.y (f)
+        var a = (long) machine.buttonA.x;
+        var b = (long) machine.buttonB.x;
+        var c = offset + machine.prize.x;
+        var d = (long) machine.buttonA.y;
+        var e = (long) machine.buttonB.y;
+        var f = offset + machine.prize.y;
+        var determinant = a * e - b * d;
+        var numX = c * e - b * f;
+        var numY = a * f - c * d;
+        return determinant != 0 && numX % determinant == 0 && numY % determinant == 0
+                ? new long[] { numX / determinant, numY / determinant } : null;
     }
 
-    private static void dfs(char[][] grid, int row, int col, int[][] classified, int id) {
-        var c = grid[row][col];
-        classified[row][col] = id;
-        for (var dir : DIRS) {
-            var adjacentRow = row + dir[0];
-            var adjacentCol = col + dir[1];
-            if (isValid(grid, adjacentRow, adjacentCol)
-                    && classified[adjacentRow][adjacentCol] == 0
-                    && grid[adjacentRow][adjacentCol] == c) {
-                dfs(grid, adjacentRow, adjacentCol, classified, id);
-            }
-        }
-    }
-
-    private static boolean isValid(char[][] grid, int row, int col) {
-        return row >= 0 && row < grid.length && col >= 0 && col < grid[row].length;
-    }
-
-    private static long sumProduct(int[] a, int[] b) {
-        var sumProduct = 0L;
-        for (var i = 0; i < a.length; i++) {
-            sumProduct += (long) a[i] * b[i];
-        }
-        return sumProduct;
-    }
+    private record Coord(int x, int y) {}
+    private record Machine(Coord buttonA, Coord buttonB, Coord prize) { }
 }
